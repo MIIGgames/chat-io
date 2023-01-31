72df7969bedf73dbab54c43fc8493fb6ae597bf9
MIIGgames <98579374+MIIGgames@users.noreply.github.com>
2023-01-31 00:46:18 -0300

    up

---
+++ server.js
@@ -0,0 +1,31 @@
+const { Socket } = require('dgram');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('wiew engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
+
+let message = [];
+
+io.on('connection', socket => {
+    console.log('Socket conectado:' + socket.id);
+
+    socket.emit('previuousMessages', message);
+
+    socket.on('sendMessage', data => {
+        message.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
+});
+
+server.listen(3000);
